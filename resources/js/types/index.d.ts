dea8c652837c8eedd0123577cdff27e2b13bef1f
Maher El Gamil <maherelgamil@gmail.com>
2024-08-12 01:09:48 +0300

    feat: init datatable , and dashboard index

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,19 +1,40 @@
 import { Config } from 'ziggy-js';
 
 export interface User {
-    id: number;
-    login: string;
-    name: string;
-    surname: string;
-    email: string;
-    identification_number: string;
-    vat: string;
-    email_verified_at: string;
+  id: number;
+  login: string;
+  name: string;
+  surname: string;
+  email: string;
+  identification_number: string;
+  vat: string;
+  email_verified_at: string;
 }
 
-export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
-    auth: {
-        user: User;
-    };
-    ziggy: Config & { location: string };
+export interface Agency {
+  id: number;
+  name: string;
+}
+
+export type PaginatedData<T> = {
+  data: T[];
+  links: {
+    first: string;
+    last: string;
+    prev: string | null;
+    next: string | null;
+  };
+};
+
+export type PageProps<
+  T extends Record<string, unknown> = Record<string, unknown>
+> = T & {
+  auth: {
+    user: User;
+  };
+  flash: {
+    success: string | null;
+    error: string | null;
+  };
+  ziggy: Config & { location: string };
 };
