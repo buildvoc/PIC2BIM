95df98af41ab3262040f62026b5cbab80e821fcb
Tanzeel <tanzeeltariq39@gmail.com>
2024-08-25 23:20:35 +0500

    fixed

--- resources/js/Pages/TaskType/Create.tsx
+++ resources/js/Pages/TaskType/Create.tsx
@@ -20,9 +20,9 @@ export function Create({ auth }: PageProps) {
   return (
     <AuthenticatedLayout
       user={user}
-      header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Create Agency</h2>}
+      header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Create Task Purpose</h2>}
     >
-      <Head title="Create Agency" />
+      <Head title="Create Task Purpose" />
       
       <div className="py-12">
         <div className="max-w-2xl mx-auto sm:px-6 lg:px-8">
@@ -64,7 +64,7 @@ export function Create({ auth }: PageProps) {
                       type="submit"
                       className="focus:outline-none flex items-center border border-indigo-600 text-indigo-600 dark:text-indigo-400 px-4 py-2 rounded-md"
                     >
-                      Create Agency
+                      Create
                     </LoadingButton>
                   </div>
                 </div>
