c1f394008428ca97ea21de26e17ba2c53bbda1d8
Aqib <aqibhussainch@gmail.com>
2024-12-12 21:45:31 +0500

    Zoom in and Zoom out problems reslved

--- resources/js/Pages/Farmers/PhotoDetail.tsx
+++ resources/js/Pages/Farmers/PhotoDetail.tsx
@@ -49,9 +49,17 @@ export function PhotoDetail({auth,photo}: PageProps) {
 
 
     return (
-<div>
+<div style={{ 
+
+  backgroundColor:'#e9ebfc'
+}}>
   <div className="fixed inset-0 z-50 flex items-end justify-center">
-    <div className="bg-white bg-opacity-60 p-5 rounded-t-lg shadow-md flex space-x-2">
+    <div className="bg-white bg-opacity-60 p-5 rounded-t-lg  flex space-x-2"
+    style={{
+      boxShadow: "0 0 3px",
+
+    }}
+    >
       <button className="rounded-md p-[3px_6px] border border-gray-500 bg-white cursor-pointer transition duration-200 ease-in-out user-select-none flex flex-1 whitespace-nowrap text-center mx-1 hover:bg-green-100" id="zoomin" onClick={handleZoomIn}>
         Zoom in
       </button>
@@ -70,7 +78,10 @@ export function PhotoDetail({auth,photo}: PageProps) {
         id="zoombox"
         ref={imgRef}
         className={`mt-1 mb-1 transition-width duration-200 ease-in-out ${imageWidth === "auto" ? "w-auto" : ""}`}
-        style={{ width: imageWidth !== "auto" ? `${imageWidth}px` : undefined }}
+        style={{
+          maxWidth:'none'
+          ,
+          width: imageWidth !== "auto" ? `${imageWidth}px` : undefined }}
         onLoad={handleImageLoad}
         src={image}
       />
