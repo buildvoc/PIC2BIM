402f33a514cc7a0f2f63db0d1132eda663bc6d7c
Tanzeel <tanzeeltariq39@gmail.com>
2024-12-13 18:15:14 +0500

    spinner enhancements

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -23,6 +23,7 @@ createInertiaApp({
         hydrateRoot(el, <App {...props} />);
     },
     progress: {
-        color: '#4B5563',
+        color: '#29d',
+        showSpinner : true
     },
 });
