ac1eceae3102054eb4eaf383b146a26d01f3a4e3
Aqib <aqibhussainch@gmail.com>
2025-01-19 13:02:14 +0500

    App.test props updated

--- resources/js/Pages/BuildingHeight/App/App.test.tsx
+++ resources/js/Pages/BuildingHeight/App/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  render(<App photos={[]} />);
+  render(<App photos={[]} isUploadTriggered={false} isMetadataResultsTriggered={false} />);
   const homeElement = screen.getByTestId("App");
   expect(homeElement).toBeInTheDocument();
 });
