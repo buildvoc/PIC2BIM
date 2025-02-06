8e9738c58a3fec90550d9cf23215f24b13ce9f2a
Tanzeel <tanzeeltariq39@gmail.com>
2025-02-07 02:05:54 +0500

    database seeders

---
+++ src/database/seeders/PaSeeder.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use App\Models\UserRole;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('pa')->updateOrInsert([
+            'name' => 'admin'
+        ],[
+            'timestamp' => now(),
+            'active' => 1
+        ]); 
+        $pa_admin = DB::table('pa')->where('name','admin')->first();
+
+        DB::table('pa')->updateOrInsert([
+            'name' => 'pic2bim'
+        ],[
+            'timestamp' => now(),
+            'active' => 1
+        ]); 
+        $pa = DB::table('pa')->where('name','pic2bim')->first();
+        
+        $roles = ['FARMER','OFFICER','SUPERADMIN'];
+        foreach($roles as $role){
+            Role::updateOrCreate([
+                'role' => $role,
+            ],[
+                'description' => $role,
+                'timestamp' => now()
+            ]);
+        }
+
+        $admin = User::updateOrCreate([
+            'login' => 'admin'
+        ],[
+            'pa_id' => $pa_admin->id,
+            'pswd' => '89e495e7941cf9e40e6980d14a16bf023ccd4c91',
+            'name' => 'Admin',
+            'email' => 'admin@mock.co.uk',
+            'active' => 1,
+            'timestamp' => now(),
+            'email_verified_at' => now()
+        ]);
+        $officer = User::updateOrCreate([
+            'login' => 'officer'
+        ],[
+            'pa_id' => $pa->id,
+            'pswd' => '89e495e7941cf9e40e6980d14a16bf023ccd4c91',
+            'name' => 'Officer',
+            'email' => 'officer@mock.co.uk',
+            'active' => 1,
+            'timestamp' => now(),
+            'email_verified_at' => now()
+        ]);
+
+        $farmer = User::updateOrCreate([
+            'login' => 'farmer'
+        ],[
+            'pa_id' => 1,
+            'pswd' => '89e495e7941cf9e40e6980d14a16bf023ccd4c91',
+            'name' => 'Farmer',
+            'email' => 'farmer@mock.co.uk',
+            'active' => 1,
+            'timestamp' => now(),
+            'email_verified_at' => now()
+        ]);
+        $adminRole = Role::where('role','SUPERADMIN')->first();
+        $officerRole = Role::where('role','OFFICER')->first();
+        $farmerRole = Role::where('role','FARMER')->first();
+
+        UserRole::updateOrCreate([
+            'user_id' => $admin->id
+        ],[
+            'role_id' => $adminRole->id,
+            'timestamp' => now()
+        ]);
+
+        UserRole::updateOrCreate([
+            'user_id' => $officer->id
+        ],[
+            'role_id' => $officerRole->id,
+            'timestamp' => now()
+        ]);
+
+        UserRole::updateOrCreate([
+            'user_id' => $farmer->id
+        ],[
+            'role_id' => $farmerRole->id,
+            'timestamp' => now()
+        ]);
+    }
+}
