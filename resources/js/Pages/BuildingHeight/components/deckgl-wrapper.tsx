3eb457025b5848f10c861f04ab1226d3569d3987
Aqib <aqibhussainch@gmail.com>
2025-02-07 16:56:01 +0500

    Basemap and terrarium added

--- resources/js/Pages/BuildingHeight/components/deckgl-wrapper.tsx
+++ resources/js/Pages/BuildingHeight/components/deckgl-wrapper.tsx
@@ -8,20 +8,31 @@ import {
   MapView,
   PickingInfo,
 } from "@deck.gl/core";
+import Map from 'react-map-gl';
 import { DeckGL } from "@deck.gl/react";
 import { MultiviewMapViewState } from "../types/map-view-state";
 import { ViewStateChangeParameters } from "@deck.gl/core";
-import { useEffect, useMemo, useState } from "react";
-import { TerrainLayer } from "@deck.gl/geo-layers";
+import { useEffect, useMemo, useState,useRef } from "react";
+import { BitmapLayer } from "@deck.gl/layers";
+import { TileLayer } from "@deck.gl/geo-layers";
+import { PMTiles } from "pmtiles";
 
+// Basemap
+import maplibregl from 'maplibre-gl';
+import { Protocol } from "pmtiles";
 import { FullscreenWidget,ZoomWidget, CompassWidget} from '@deck.gl/widgets';
 import '@deck.gl/widgets/stylesheet.css';
+import { sample } from "turf";
+import { map } from "jquery";
 interface DeckglWrapperProps {
   parentViewState: MultiviewMapViewState | null;
   view: "firstPerson" | "map" | "orthographic";
   layers: Layer[];
   onHover: (info: PickingInfo) => void;
 }
+
+const PMTILES_URL =
+  "https://r2-public.protomaps.com/protomaps-sample-datasets/terrarium_z9.pmtiles";
 
 export const DeckglWrapper = ({
   parentViewState,
@@ -46,8 +57,11 @@ export const DeckglWrapper = ({
       bearing: 0,
     },
   });
-  const [commonLayers, setCommonLayers] = useState<Layer[]>([]);
 
+
+  //BaseMap
+  const mapRef = useRef<maplibregl.Map | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
     if (parentViewState !== null) {
       setViewState(parentViewState);
@@ -78,31 +92,99 @@ export const DeckglWrapper = ({
     // eslint-disable-next-line
   }, [view]);
 
+  // useEffect(() => {
+  //   const layer = new TileLayer({
+  //     id: 'TileLayer',
+  //     data: 'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  //     maxZoom: 19,
+  //     minZoom: 0,
+    
+  //     renderSubLayers: props => {
+  //       const {boundingBox} = props.tile;
+    
+  //       return new BitmapLayer(props, {
+  //         data: null,
+  //         image: props.data,
+  //         bounds: [boundingBox[0][0], boundingBox[0][1], boundingBox[1][0], boundingBox[1][1]]
+  //       });
+  //     },
+  //     pickable: true
+  //   });
+  //     setCommonLayers((res)=>[...res,layer]);
+  // }, []);
+
+
+  // useEffect(()=>{
+  //   const URL = "https://r2-public.protomaps.com/protomaps-sample-datasets/terrarium_z9.pmtiles"
+  //   let tileSource = createDataSource(URL
+  //     ,
+  //     [PMTilesSource, MVTSource], {}
+  // )
+  // const tileLayer = new TileSourceLayer({ tileSource });
+  // setTerrainLayer([tileLayer])
+  // },[])
+
+
   useEffect(() => {
-    const deckglTerrainLayer = new TerrainLayer({
-      id: "terrain",
-      maxZoom: 21,
-      elevationDecoder: {
-        rScaler: 6553.6,
-        gScaler: 25.6,
-        bScaler: 0.1,
-        offset: -10000,
-      },
-      loadOptions: {
-        terrain: {
-          tesselator: "martini",
-          skirtHeight: 50,
-        },
-      },
-      // Digital elevation model from https://www.usgs.gov/
-      elevationData:
-        "https://tiles.buildingshistory.co.uk/geoserver/gwc/service/tms/1.0.0/dem%3ARGB_Terrain@WebMercatorQuad@png/{z}/{x}/{y}.png?flipY=true",
-      texture:
-        "https://tiles.buildingshistory.co.uk/geoserver/gwc/service/tms/1.0.0/buildings%3AOutdoor_3857@WebMercatorQuad@png/{z}/{x}/{y}.png?flipY=true",
-      meshMaxError: 0.6,
-    });
-    setCommonLayers([deckglTerrainLayer]);
-  }, []);
+    if(mapContainerRef.current)
+    {
+      let protocol = new Protocol();
+      maplibregl.addProtocol("pmtiles", protocol.tile);
+       mapRef.current = new maplibregl.Map({
+        container: mapContainerRef.current,
+        style: "https://tiles.openfreemap.org/styles/liberty",
+        center: [-0.7960, 51.2140], 
+        zoom: 16,
+        pitch: 61,
+        bearing: 0,
+        maxPitch: 85,
+        maxZoom: 20
+      });
+      console.log("Map call ----------->")
+      mapRef.current.on("load", () => {
+        mapRef.current?.addSource('terrainSource', {
+          type: "raster-dem",
+          url: "pmtiles://" + PMTILES_URL,
+          tileSize: 256,
+          encoding:"terrarium"
+      });
+      mapRef.current?.addSource('hillshadeSource', {
+        type: "raster-dem",
+        url: "pmtiles://" + PMTILES_URL,
+        tileSize: 256,
+      });
+
+  
+      mapRef.current?.addLayer({
+            id: 'hillshadeLayer',
+            type: 'hillshade',
+            source: 'terrainSource',
+            paint: {
+                'hillshade-shadow-color': '#000000', 
+                'hillshade-highlight-color': '#ffffff',
+                'hillshade-accent-color': '#888888'
+            }
+        });
+      });
+
+      mapRef.current?.addControl(
+        new maplibregl.NavigationControl({
+          visualizePitch: true,
+          showZoom: true,
+          showCompass: true,
+        })
+      );
+
+      mapRef.current?.addControl(
+        new maplibregl.TerrainControl({
+          source: "terrainSource",
+          exaggeration: 1
+        }))
+    }
+
+
+    return () => mapRef.current?.remove();
+  }, [mapContainerRef.current]);
 
 
   const onViewStateChangeHandler = (parameters: ViewStateChangeParameters) => {
@@ -130,7 +212,7 @@ export const DeckglWrapper = ({
         },
         firstPersonView: deckViewState,
       };
-    }
+    } 
     setViewState(newViewState);
   };
 
@@ -149,6 +231,7 @@ export const DeckglWrapper = ({
               altitude: 10,
               orthographic: view === "orthographic",
             }),
+
           ]
         : [
             new FirstPersonView({
@@ -170,8 +253,8 @@ export const DeckglWrapper = ({
       }
       onViewStateChange={onViewStateChangeHandler}
       onHover={onHover}
-      layers={[...commonLayers, ...layers]}
       views={VIEWS}
+      controller={true}
       widgets={view !== "firstPerson"? [
         new FullscreenWidget({
           placement:"top-right",
@@ -191,8 +274,10 @@ export const DeckglWrapper = ({
           placement:"top-right",
           style:{top:"150px",position:"absolute",right:"5px" }
         })
+
       ]:[]}
-      
+      style={{ width: "100vw", height: "100vh" }}
+
       effects={[
         new LightingEffect({
           ambientLight: new AmbientLight({
@@ -201,6 +286,12 @@ export const DeckglWrapper = ({
           }),
         }),
       ]}
-    />
+      layers={[ ...layers]}
+
+    >
+
+<div ref={mapContainerRef} style={{ width: "100vw", height: "100vh" }}  />
+      </DeckGL>
+  
   );
 };
