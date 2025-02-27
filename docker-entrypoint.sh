01657e4d7c288551c26d80f47669b0b26d9027f0
Tanzeel <tanzeeltariq39@gmail.com>
2025-02-27 16:14:36 +0500

    updated docker build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,7 +18,7 @@ php artisan config:clear
 php artisan migrate --force
 php artisan db:seed
 
-sed -i 's|DocumentRoot /var/www/html|DocumentRoot /var/www/html/public|g' /etc/apache2/sites-available/000-default.conf
+sed -i 's|DocumentRoot .*$|DocumentRoot /var/www/html/public|g' /etc/apache2/sites-available/000-default.conf
 
 # Start Apache
 apache2-foreground
