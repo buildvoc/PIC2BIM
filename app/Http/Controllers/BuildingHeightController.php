b9424d5adbe3ecf71d3cd072147c1fff23b3b95f
Tanzeel <tanzeeltariq39@gmail.com>
2025-03-13 16:31:31 +0500

    update staging branch

--- app/Http/Controllers/BuildingHeightController.php
+++ app/Http/Controllers/BuildingHeightController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\File;
 
  class BuildingHeightController extends Controller
 {
@@ -15,5 +15,22 @@ use Illuminate\Support\Facades\Auth;
         return Inertia::render('BuildingHeight/Index',compact('photos'));
     }
 
+    public function lazFiles(){
+        $path = public_path('laz');
+
+        if (!File::exists($path)) {
+            return response()->json(['error' => 'Directory not found'], 404);
+        }
+
+        $files = File::files($path);
+
+        $fileList = array_map(function ($file) {
+            return [
+                'name'  => $file->getFilename(),
+            ];
+        }, $files);
+
+        return response()->json($fileList, 200, ['Content-Type' => 'application/json']);
+    }
 
 }
