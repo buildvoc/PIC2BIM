0e3277a7964c69bc192a3a46e541a23323bd947b
Tanzeel <tanzeeltariq39@gmail.com>
2025-05-02 18:35:45 +0500

    verifying queued functionality

--- app/Http/Controllers/PhotoGalleryController.php
+++ app/Http/Controllers/PhotoGalleryController.php
@@ -6,12 +6,15 @@ use Inertia\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
-
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 class PhotoGalleryController extends Controller
 {
     public function index(Request $request)
     {
+        Artisan::queue('app:cron-check-location');
+        Log::info("QUEUED THE COMMAND");
         $user = Auth::user();
         $photos =  getPhotosWithoutTask($user->id);
         return Inertia::render('Farmers/PhotoGallery', compact('photos'));
