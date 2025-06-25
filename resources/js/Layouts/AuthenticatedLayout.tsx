487ec1a6b2dfdc013399dd7a8ddefabc39a77130
Cahyo Saputro <cahyoortupas@gmail.com>
2025-06-25 19:00:56 +0700

    Adjust task page layout

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -463,7 +463,7 @@ export default function Authenticated({
 
             {header && (
                 <header className="bg-white dark:bg-gray-800 shadow">
-                    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
                         {header}
                     </div>
                 </header>
